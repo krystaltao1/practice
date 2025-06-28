57abec0454f7346cd1a2b42021654f39f1caa9fa
Krystal <krystaltaoau@gmail.com>
2025-06-28 19:46:32 +1000

    refactor: setup practice project

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  it("renders correctly", () => {
+    render(<App />);
+    expect(screen.getByText(/what happen/i)).toBeInTheDocument();
+  });
+});
